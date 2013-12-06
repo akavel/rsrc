c61a64e676d720c36cbf7bb96384b9d1ed284eef
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2013-12-07 00:17:36 +0100

    read manifest file

--- rsrc.go
+++ rsrc.go
@@ -3,7 +3,9 @@ package main
 import (
 	//"debug/pe"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,10 +17,27 @@ func main() {
 }
 
 func run() error {
+	//TODO: allow options:
+	// -o FILENAME - output file name
+	// + advanced specification of multiple resources, as a tree (json?)
 	if len(os.Args) <= 1 {
 		return fmt.Errorf("USAGE: %s FILE.exe.manifest\n"+
 			"Generates FILE.res",
 			os.Args[0])
 	}
+
+	fname := os.Args[1]
+	suffix := ".exe.manifest"
+	if !strings.HasSuffix(fname, suffix) {
+		return fmt.Errorf("Filename '%s' does not end in suffix '%s'", fname, suffix)
+	}
+	fname = fname[:len(fname)-len(suffix)]
+
+	manifest, err := ioutil.ReadFile(fname + suffix)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(manifest))
+
 	return nil
 }
