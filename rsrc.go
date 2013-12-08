8b985fa687a26f56b24e3cc53d5fa78b35f6f5c8
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2013-12-09 00:18:12 +0100

    adjusting usage info to hopefully better explain

--- rsrc.go
+++ rsrc.go
@@ -89,12 +89,13 @@ func main() {
 	//TODO: allow in options advanced specification of multiple resources, as a tree (json?)
 	var fnamein, fnameout string
 	flags := flag.NewFlagSet("", flag.ContinueOnError)
-	flags.StringVar(&fnamein, "manifest", "", "REQUIRED: path to Windows manifest file")
+	flags.StringVar(&fnamein, "manifest", "", "path to a Windows manifest file to embed")
 	flags.StringVar(&fnameout, "o", "rsrc.syso", "name of output COFF (.res or .syso) file")
 	_ = flags.Parse(os.Args[1:])
 	if fnamein == "" {
 		fmt.Fprintf(os.Stderr, "USAGE: %s -manifest FILE.exe.manifest [-o FILE.syso]\n"+
-			"Generates a .syso file with resources in .rsrc section, for consumption by Go linker.\n"+
+			"Generates a .syso file with specified resources embedded in .rsrc section,\n"+
+			"aimed for consumption by Go linker when building Win32 excecutables.\n"+
 			"OPTIONS:\n",
 			os.Args[0])
 		flags.PrintDefaults()
